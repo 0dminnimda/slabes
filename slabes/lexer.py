3d4ae3128fba34b96e5e75d3be9e2d83fda1e0f7
0dminnimda <0dminnimda@gmail.com>
2024-02-15 05:18:31 +0300

    Disallow underscores, give amazing errors on names n numbers

--- slabes/lexer.py
+++ slabes/lexer.py
@@ -1,3 +1,4 @@
+import re
 import token
 from tokenize import TokenInfo
 from itertools import accumulate
@@ -13,11 +14,21 @@ class Lexer:
         self.reset("", "")
 
     def reset(self, text: str, filename: str):
+        self.text = text
         self.lines = text.splitlines()
         self.cumlen = [0] + list(accumulate((len(it) + 1 for it in self.lines)))
         self.filename = filename
-        self.ply_lexer.input(text)
+        self.reset_ply(text, "INITIAL")
 
+    def reset_ply(self, text: str, state: str):
+        self.ply_lexer.begin(state)
+        self.ply_lexer.input(text)
+        self.ply_lexer.lineno = 1
+
+    def skip_ply_by(self, count: int):
+        lexpos = self.ply_lexer.lexpos
+        self.ply_lexer.lexpos += count
+        self.ply_lexer.lineno += self.text.count("\n", lexpos, self.ply_lexer.lexpos)
 
     token_name_to_type = {
         "NAME": token.NAME,
@@ -25,34 +36,55 @@ class Lexer:
         "COMMENT": token.COMMENT,
     }
 
+    states = (("INVALID", "exclusive"),)
 
     tokens = list(token_name_to_type.keys())
 
-
     literals = [",", ".", "=", "+", "-", "*", "/", "(", ")"]
 
+    t_INITIAL_NAME = r"\b[a-z_][a-z0-9_]*\b"
+    t_INITIAL_NUMBER = r"\b(?!_)[A-W0-9_]+(?<!_)\b"
 
-    t_NAME = r"[a-z_][a-z0-9_]*"
-    t_NUMBER = r"[A-W0-9_]+"
+    t_ANY_COMMENT = r"\#.*"
+    t_ANY_ignore = " \t"
 
-    t_COMMENT = r"\#.*"
-    t_ignore = " \t"
+    def t_INVALID_NAME_NUMBER(self, t):
+        r"\b[a-wA-W0-9_]+\b"
+        tok = self.ply_token_to_py(t)
+        loc = Location.from_token(self.filename, tok)
+        if tok.string[0].isdigit() and not tok.string.isupper():
+            msg = f"numbers have to be uppercase. Did you mean '{tok.string.upper()}'?"
+        elif tok.string.startswith("_"):
+            msg = f"numbers cannot start with an underscore. Did you mean '{tok.string.lstrip('_')}'?"
+        elif not tok.string[0].isdigit() and not tok.string.islower():
+            msg = f"names have to be lowercase. Did you mean '{tok.string.lower()}'?"
+        elif not tok.string.isupper() and not tok.string.islower():
+            # XXX: is it even reachable?
+            msg = "numbers have to be uppercase and names have to be lowercase"
+        elif tok.string.endswith("_"):
+            msg = f"numbers cannot end with an underscore. Did you mean '{tok.string.rstrip('_')}'?"
+        else:
+            msg = "invalid number or name"
+        report_fatal_at(loc, "SyntaxError", msg, tok.line)
 
-
-
-
-
-    def t_newline(self, t):
+    def t_ANY_newline(self, t):
         r"\n+"
         t.lexer.lineno += t.value.count("\n")
 
-    def t_error(self, t):
+    def t_INVALID_error(self, t):
         tok = self.ply_token_to_py(t)
         loc = Location.from_token(self.filename, tok)
         # point to just one character because error token goes to the end of the string
         loc = loc.without_end()
         msg = "illegal combination of characters"
         report_fatal_at(loc, "SyntaxError", msg, tok.line)
+
+    def t_INITIAL_error(self, t):
+        self.reset_ply(self.text, "INVALID")
+        self.skip_ply_by(t.lexpos)
+        for tok in self.ply_lexer:
+            pass  # should error
+        assert False, "error state passed without error"
 
     def ply_token_to_py(self, tok):
         string = tok.value
@@ -80,4 +112,6 @@ def lex(text: str, filename: str = "<unknown>"):
 
     for tok in _lexer.ply_lexer:
         yield _lexer.ply_token_to_py(tok)
-    yield TokenInfo(token.ENDMARKER, "", (len(_lexer.lines) + 1, 0), (len(_lexer.lines) + 1, 0), "")
+    yield TokenInfo(
+        token.ENDMARKER, "", (len(_lexer.lines) + 1, 0), (len(_lexer.lines) + 1, 0), ""
+    )
