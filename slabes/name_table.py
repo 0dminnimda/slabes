057e6b71598dbbce6d73eb5c2ba7c1c78638cf87
0dminnimda <0dminnimda@gmail.com>
2024-02-21 23:38:44 +0300

    Create name_table.py

---
+++ slabes/name_table.py
@@ -0,0 +1,69 @@
+from __future__ import annotations
+
+from dataclasses import dataclass, field
+from typing import Iterable, TypeVar, Generic
+
+from . import ast_nodes as ast
+
+
+@dataclass
+class NameTable:
+    """
+    This structure is for anwering those questions:
+    - Does this name exists here? 
+    - Is it accessible?
+    - Where did it come from?
+    """
+
+    outer: NameTable | None = None
+    local_names: set[str] = field(default_factory=set)
+
+
+NameTableT = TypeVar("NameTableT", bound=NameTable)
+
+
+@dataclass
+class NameTableVisitor(ast.Visitor, Generic[NameTableT]):
+    table: NameTableT
+
+    def declare_name(self, name: str) -> None:
+        self.table.local_names.add(name)
+
+    def visit_Function(self, node: ast.Function) -> None:
+        self.declare_name(node.name)
+
+    def visit_ArrayDeclaration(self, node: ast.ArrayDeclaration) -> None:
+        for name in node.names:
+            self.declare_name(name.value)
+
+    def visit_NumberDeclaration(self, node: ast.NumberDeclaration) -> None:
+        for name in node.names:
+            self.declare_name(name.value)
+
+    def visit_Argument(self, node: ast.Argument) -> None:
+        self.declare_name(node.name.value)
+
+    def visit_Check(self, node: ast.Check) -> None:
+        pass
+
+    def visit_Until(self, node: ast.Until) -> None:
+        pass
+
+
+def fill_name_table_from_ast(table: NameTableT, node: ast.AST) -> None:
+    NameTableVisitor(table).visit(node)
+
+
+def walk_up_name_tables(table: NameTableT | None) -> Iterable[NameTableT]:
+    while table is not None:
+        yield table
+        table = table.outer
+
+
+def lookup_origin(table: NameTableT | None, name: str) -> NameTableT | None:
+    start = table
+    for table in walk_up_name_tables(start):
+        if name in table.local_names:
+            return table
+
+    return None
