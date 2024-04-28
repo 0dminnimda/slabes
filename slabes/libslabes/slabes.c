ecb617a431f52f1b8a7015d35c725ed2bc492b94
0dminnimda <0dminnimda@gmail.com>
2024-04-28 16:27:06 +0300

    Split slabes.c to also slabes.h and slabes_console.c

--- slabes/libslabes/slabes.c
+++ slabes/libslabes/slabes.c
@@ -5,9 +5,9 @@
 //   return a + b;
 // }
 
+#include "slabes.h"
+
 #include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
 #include <string.h>
 
 
@@ -28,45 +28,10 @@ void *slabes_malloc_(size_t size) {
 #endif // SLABES_FREE
 
 
-/*
- _   _   _  
-/0\_/1\_/2\_
-\_/3\_/4\_/5\
-/6\_/7\_/8\_/
-\_/ \_/ \_/ 
-
-*/
-
-typedef enum : char {
-    Wall = '#',
-    Player = 'o',
-    Empty = ' ',
-} Cell;
-
-typedef struct {
-    size_t width, height;
-    Cell *cells;
-} Field;
-
-typedef enum {
-    UpLeft,
-    Up,
-    UpRight,
-    DownRight,
-    Down,
-    DownLeft,
-} Direction;
-
-typedef struct {
-    size_t x, y;
-} Position;
-
-typedef struct {
-    Position player_position;
-    Direction player_direction;
-    Field field;
-} Game;
-
+Game *get_game() {
+    static Game game;
+    return &game;
+}
 
 char *direction_to_string(Direction direction) {
     switch (direction) {
@@ -79,8 +44,6 @@ char *direction_to_string(Direction direction) {
         default: return "Unknown";
     }
 }
-
-#define FIELD_AT(field, x, y) (field)->cells[(y) * (field)->width + (x)]
 
 Cell field_check_at(Field *field, ssize_t x, ssize_t y, Cell default_value) {
     if (x >= field->width || x < 0) {
@@ -197,193 +160,4 @@ bool game_make_player_take_one_step(Game *game) {
     }
     game_set_player_position(game, pos);
     return true;
-}
-
-/*
- _   _   _  
-/#\_/ \_/ \_
-\#/ \_/#\_/ \
-/ \_/#\#/o\_/
-\_/ \#/ \_/ 
-
- _   _   _  
-/#\_/ \_/o\_
-\#/ \_/#\_/ \
-  \_/ \#/ \_/
-
- __    __    __  
-/##\__/  \__/  \__
-\##/  \__/##\ _/  \
-/  \__/##\##/oo\__/
-\__/  \##/  \__/ 
-
-*/
-
-
-char player_upper_char(Game *game) {
-    if (game->player_direction == Up) {
-        return '^';
-    } else if (game->player_direction == UpRight) {
-        return '>';
-    } else if (game->player_direction == UpLeft) {
-        return '<';
-    }
-    return ' ';
-}
-
-char player_lower_char(Game *game) {
-    if (game->player_direction == Down) {
-        return 'v';
-    } else if (game->player_direction == DownRight) {
-        return '>';
-    } else if (game->player_direction == DownLeft) {
-        return '<';
-    }
-    return '_';
-}
-
-
-void game_print_small_upper_row(Game *game, ssize_t y, bool wide) {
-    char c0 = y? '/' : ' ';
-    for (ssize_t x = 0; x < game->field.width; x++) {
-        char c1 = field_check_at(&game->field, x, y - 1, ' ');
-        if (c1 == Player) { c1 = player_upper_char(game); }
-        char c2 = field_check_at(&game->field, x, y, ' ');
-        if (c2 == Empty) { c2 = '_'; }
-        if (c2 == Player) { c2 = player_lower_char(game); }
-        if (wide) {
-            printf("%c%c%c\\%c%c", c0, c1, c1, c2, c2);
-        } else {
-            printf("%c%c\\%c", c0, c1, c2);
-        }
-        c0 = '/';
-    }
-    if (y != (game->field.height)) { printf("/"); }
-    printf("\n");
-}
-
-void game_print_small_lower_row(Game *game, ssize_t y, bool wide) {
-    char c0 = (y < game->field.height)? '\\' : ' ';
-    for (ssize_t x = 0; x < game->field.width; x++) {
-        char c1 = field_check_at(&game->field, x, y , ' ');
-        if (c1 == Empty) { c1 = '_'; }
-        if (c1 == Player) { c1 = player_lower_char(game); }
-        char c2 = field_check_at(&game->field, x, y - 1, ' ');
-        if (c2 == Player) { c2 = player_upper_char(game); }
-        if (wide) {
-            printf("%c%c%c/%c%c", c0, c1, c1, c2, c2);
-        } else {
-            printf("%c%c/%c", c0, c1, c2);
-        }
-        c0 = '\\';
-    }
-    if (y) { printf("\\"); }
-    printf("\n");
-}
-
-void game_print_small(Game *game, bool wide) {
-    if (game->field.height == 0 || game->field.width == 0) {
-        printf("<empty game field>\n");
-        return;
-    }
-
-    char *first = wide? " __   " : " _  ";
-    if (game->field.height % 2 == 0) {
-        first = wide? "   __ " : "  _ ";
-        printf(" ");
-    }
-
-    for (ssize_t x = 0; x < game->field.width; x++) { printf("%s", first); }
-    printf("\n");
-
-    ssize_t y = game->field.height;
-    if (game->field.height % 2 == 0) {
-        for (;;) {
-            game_print_small_lower_row(game, y, wide);
-            if (--y < 0) break;
-            game_print_small_upper_row(game, y, wide);
-            if (--y < 0) break;
-        }
-    } else {
-        for (;;) {
-            game_print_small_upper_row(game, y, wide);
-            if (--y < 0) break;
-            game_print_small_lower_row(game, y, wide);
-            if (--y < 0) break;
-        }
-    }
-}
-
-/*
-  _   _   _   _    
- / \_/ \_/#\_/ \
-|   |   |###|   |
- \_/ \_/ \#/#\_/ \
-  |   |   |###|   |
- /#\_/ \_/ \#/ \_/
-|###|   | O |   |
- \#/ \_/ \_/ \_/
-
-  __    __    __    __    
- /  \__/  \__/##\__/  \
-|    ||    ||####||    ||
- \__/  \__/  \##/##\__/  \
-  ||    ||    ||####||    |
- /##\__/  \__/  \##/  \__/
-|####||    || () ||    ||
- \##/  \__/  \__/  \__/
-
-  ___     ___     ___     ___    
- /   \ _ /   \ _ /###\___/   \
-|     |_|     |_|#####|_|     |__
- \___/   \___/   \###/###\___/   \
-  |_|     |_|     |_|#####|_|     |
- /###\___/   \___/   \###/   \___/
-|#####|_|     |_|  @  |_|     |
- \###/   \___/   \___/   \___/
-
-*/
-
-static Game game;
-
-int main() {
-    field_construct_square(&game.field, 10);
-
-    game_reset(&game);
-
-    game_set_player_position(&game, (Position){0, 0});
-
-    FIELD_AT(&game.field, 1, 0) = Wall;
-    FIELD_AT(&game.field, 1, 1) = Wall;
-    FIELD_AT(&game.field, 2, 1) = Wall;
-    FIELD_AT(&game.field, 2, 2) = Wall;
-    FIELD_AT(&game.field, 3, 0) = Wall;
-
-    game_print_small(&game, true);
-
-    while (1) {
-        printf("> ");
-        fflush(stdout);
-        char c = getchar();
-        if (c == 'x') { break; }
-        bool move = true;
-        switch (c) {
-            case 'w': game.player_direction = Up; break;
-            case 's': game.player_direction = Down; break;
-            case 'e': game.player_direction = UpRight; break;
-            case 'd': game.player_direction = DownRight; break;
-            case 'q': game.player_direction = UpLeft; break;
-            case 'a': game.player_direction = DownLeft; break;
-            default: move = false; break;
-        }
-
-        if (move) {
-            if (!game_make_player_take_one_step(&game)) continue;
-            game_print_small(&game, true);
-        }
-    }
-
-    field_destruct(&game.field);
-
-    return 0;
 }
---
+++ slabes/libslabes/slabes.h
@@ -0,0 +1,50 @@
+#include <stdbool.h>
+#include <stdlib.h>
+
+/*
+ _   _   _  
+/0\_/1\_/2\_
+\_/3\_/4\_/5\
+/6\_/7\_/8\_/
+\_/ \_/ \_/ 
+
+*/
+
+typedef enum : char {
+    Wall = '#',
+    Player = 'o',
+    Empty = ' ',
+} Cell;
+
+typedef struct {
+    size_t width, height;
+    Cell *cells;
+} Field;
+
+typedef enum {
+    UpLeft,
+    Up,
+    UpRight,
+    DownRight,
+    Down,
+    DownLeft,
+} Direction;
+
+typedef struct {
+    size_t x, y;
+} Position;
+
+typedef struct {
+    Position player_position;
+    Direction player_direction;
+    Field field;
+} Game;
+
+
+char *direction_to_string(Direction direction);
+
+#define FIELD_AT(field, x, y) (field)->cells[(y) * (field)->width + (x)]
+
+Cell field_check_at(Field *field, ssize_t x, ssize_t y, Cell default_value);
+
+Game *get_game();
