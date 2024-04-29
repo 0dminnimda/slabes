e8b70b19819046e132f42e1f89d931fb672a9d95
0dminnimda <0dminnimda@gmail.com>
2024-04-29 11:33:46 +0300

    Add android support, fix raylib drawing index order

--- Makefile
+++ Makefile
@@ -2,6 +2,9 @@ PYTHON = "$(shell (which python3 2>/dev/null || which python 2>/dev/null) | head
 
 prep_executable = $(eval $(1) := $(shell bash ./prep_exec.sh $(2)))
 
+DISPLAY = ":1"
+NO_DISPLAY = ":0"
+
 .PHONY: install
 install:
 	python3 -m pip install -e .
@@ -21,8 +24,25 @@ ralib_display_lib:
 
 .PHONY: run
 run:
-	$(call prep_executable, EXEC, ./tests/compile/all.out)
+	$(call prep_executable, EXEC, ./tests/compile/robot.out)
 	$(EXEC)
+
+.PHONY: run_con
+run_con:
+	$(call prep_executable, SLIB, ./slabes/libslabes/console_display.so)
+	$(call prep_executable, EXEC, ./tests/compile/robot.out)
+	$(EXEC) $(SLIB)
+
+.PHONY: run_ray
+run_ray:
+	$(call prep_executable, SLIB, ./slabes/libslabes/raylib_display.so)
+	$(call prep_executable, EXEC, ./tests/compile/robot.out)
+	DISPLAY=$(DISPLAY) $(EXEC) $(SLIB)
+
+# https://wiki.termux.com/wiki/Graphical_Environment
+.PHONY: setup_termux_gui
+setup_termux_gui:
+	vncserver :1 -localhost
 
 .PHONY: generate_parser
 generate_parser:
--- slabes/libslabes/raylib_display.c
+++ slabes/libslabes/raylib_display.c
@@ -3,6 +3,7 @@
 #include <math.h>
 
 typedef struct {
+    float elapsed_time;
 } State;
 
 State state;
@@ -10,24 +11,40 @@ State state;
 const int screenWidth = 800;
 const int screenHeight = 450;
 
-const double s = 30;  // Side length of the hexagon
+const double s = 20;  // Side length of the hexagon
 const int num_rows = 8;  // Number of rows
 const int num_cols = 12;  // Number of columns
 const int offsetX = 50;  // Horizontal offset to center the grid
 const int offsetY = 50;  // Vertical offset to center the grid
 
+const Color empty_color = DARKBLUE;
+const Color wall_color = GRAY;
+const Color player_color = WHITE;
 
-void draw_hexagon_grid(double s, int num_rows, int num_cols, int offsetX, int offsetY) {
-    double height = 1.5 * s;  // Vertical distance between rows
-    double width = sqrt(3) * s;  // Horizontal distance between hexagon centers in a row
+Color cell_color(Cell cell) {
+    switch (cell) {
+        case Player: return player_color;
+        case Wall: return wall_color;
+        default: return empty_color;
+    }
+}
 
-    for (int row = 0; row < num_rows; row++) {
-        for (int col = 0; col < num_cols; col++) {
-            double x = col * width + (row % 2) * (width / 2) + offsetX;
-            double y = row * height + offsetY;
-            
+
+void draw_hexagon_grid(Game *game, double s) {
+    double x_delta = 3 * s;  // distance between hexagons in the same row
+    double y_delta = sqrt(3) / 2 * s;  // distance between rows
+
+    char *msg[16] = {0};
+
+    for (ssize_t yi = 0; yi < game->field.height - 1; yi++) {
+        for (ssize_t xi = 0; xi < game->field.width - 1; xi++) {
+            double y = yi * y_delta + offsetX;
+            double x = xi * x_delta + (yi % 2) * (x_delta / 2) + offsetY;
+
+            sprintf(msg, "%d,%d", (int)yi, (int)xi);
             Vector2 center = {x, y};
-            DrawPoly(center, 6, s, 0.0f, DARKBLUE);  // Draw a hexagon
+            DrawPoly(center, 6, s * 0.95, 30.0f, DARKBLUE);
+            DrawText(msg, x - x_delta/4, y - y_delta/4, 10, DARKGRAY);
         }
     }
 }
@@ -42,10 +59,14 @@ bool setup_display() {
 }
 
 void update_display(Game *game) {
+    state.elapsed_time += GetFrameTime();
+
     if (!WindowShouldClose()) {
         BeginDrawing();
 
-        draw_hexagon_grid(s, num_rows, num_cols, offsetX, offsetY);
+        ClearBackground(BLACK);
+
+        draw_hexagon_grid(game, s);
 
         EndDrawing();
     }
@@ -53,5 +74,16 @@ void update_display(Game *game) {
 }
 
 void cleanup_display() {
+    while (!WindowShouldClose()) {
+        state.elapsed_time += GetFrameTime();
+
+        BeginDrawing();
+
+        // ClearBackground(BLACK);
+
+        // draw_hexagon_grid(game, s);
+
+        EndDrawing();
+    }
     CloseWindow();
 }
